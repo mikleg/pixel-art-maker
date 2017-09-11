d3bcf7925a787fb69f52b6059a2a301106646092
Mikle Gorbunov <mihailgorbunov74@gmail.com>
2017-09-10 21:13:47 -0700

    very basic application

--- pixel-art.js
+++ pixel-art.js
@@ -1,35 +1,96 @@
+    var currentColor = "red"; //the current color for drawing
+    var allcolors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue" // some colors for palette
+        ,"BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk"];
+        //,"Crimson"
+        //,"Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen"
+        //,"Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey"
+    // ,"DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite"
+    // ,"ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew"
+    // ,"HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue"
+    // ,"LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon"
+    // ,"LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen"
+    // ,"Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue"
+    // ,"MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite"
+    // ,"Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed"
+    // ,"PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon"
+    // ,"SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen"
+    // ,"SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+    drawCanvas(20,30, document.getElementById("canvas"));
+    drawPalette(allcolors);
+    drawCurrentColor();
 
-printCanvas();
-//function printCanvas(height, width, symbol) {
-function printCanvas() {
 
+    function getCurrentColor() {
+        return currentColor;
+     }
 
-    /*
-        for (var j = 0; j < height; j++) {
-            var rez = '';
-            for (var i = 0; i < (height - (1+j)); i++)
-                rez = rez + String.fromCharCode(160);
-            for (i = 0; i < j+2 ; i++)
-                rez = rez + symbol;
-            var node = document.createElement("P");
-            var textnode = document.createTextNode(rez);
-            node.appendChild(textnode);
-            document.getElementById("pyramid").appendChild(node);
+    function setCurrentColor(color) {
+        currentColor = color;
+    }
+    //draws current color point
+    function drawCurrentColor(){
+        drawCanvas(1,1, document.getElementById("currentColor"));
+        changePoint (currentColor, document.getElementById("currentColor_r0c0"), "black");
+    }
 
+    //draws a palette with the given array of colors
+    function drawPalette(colors){
+        drawCanvas(1,colors.length, document.getElementById("palette"));
+        for (var i = 0; i < colors.length; i++){
+            changePoint (colors[i], document.getElementById("palette_r0c" + i), "black");
         }
-    */
-    var node = document.createElement("DIV");
-    var att = document.createAttribute("background-color");
-    att.value = "red";
-    node.setAttributeNode(att);
-    att = document.createAttribute("border");
-    att.value = "5px solid green";
-    node.setAttributeNode(att);
-    var textnode = document.createTextNode("     ");
-    node.appendChild(textnode);
-    document.getElementById("canvas").appendChild(node);
+    }
+    //sets a current color if it called from the palette or changes a color of point in other case
+    function clickEvent(node){
+        var parent = node.parentNode;
+        var grandParent = parent.parentNode;
+        if (grandParent.getAttribute("id") == "palette"){
+            setCurrentColor(window.getComputedStyle(node).backgroundColor);
+            changePoint (getCurrentColor(), document.getElementById("currentColor_r0c0"), "black");
+        }
+        if (grandParent.getAttribute("id") == "canvas"){
+            changePoint (getCurrentColor(), node, "black");
+        }
 
+    }
+    //draws a canvas with given size and background color
+    function drawCanvas(rows, columns, parent, borderColor)
+    {
+        if (borderColor == undefined)
+            borderColor = "black";
+        for (var i = 0; i < rows; i++) {
+            var row = document.createElement("DIV");
+            for (var j = 0; j < columns; j++){
+                row.appendChild(makePoint("white",parent.getAttribute("id")+"_r"+ i +"c" + j, borderColor));
+            }
+        parent.appendChild(row);
+        }
+    }
 
+// changes color and borderColor attributes
+    function changePoint (color, node, borderColor){
+        node.setAttribute("class", "whitePixel");
+        if (color != "white")
+            node.style.background = color;
+        if (borderColor != "black")
+            node.style.borderColor = borderColor;
+        node.addEventListener("click", function(){
+            //console.log("event " + "row= " + row + " column=" + column);
+            //console.log("border-color= " + window.getComputedStyle(node).backgroundColor);
+           // changePoint (getCurrentColor(), node, window.getComputedStyle(node).backgroundColor);
+           // changePoint ("red", row, column, node, "green");
+            clickEvent(node);
+        });
+        return node;
+    }
 
+// Makes a point (node) with given id, color and borderColor
+    function makePoint (color, id, borderColor){
+        var node = document.createElement("DIV");
+        node.setAttribute("id", id);
+        changePoint(color, node, borderColor);
+        const textnode = document.createTextNode("T");
+        node.appendChild(textnode);
+        return node;
+    }
 
-}
